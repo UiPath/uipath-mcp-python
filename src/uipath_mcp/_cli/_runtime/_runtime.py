605dac3c7fa4343429cbb622712d8ad3819ff60d
Cristian Pufu <cristian.pufu@uipath.com>
2025-07-22 08:08:26 +0200

    fix: pass access token on SignalR connect

--- src/uipath_mcp/_cli/_runtime/_runtime.py
+++ src/uipath_mcp/_cli/_runtime/_runtime.py
@@ -176,8 +176,12 @@ class UiPathMcpRuntime(UiPathBaseRuntime):
                 root_span.set_attribute("args", json.dumps(self._server.args))
                 root_span.set_attribute("span_type", "MCP Server")
 
+                def token_factory() -> str:
+                    return os.environ.get("UIPATH_ACCESS_TOKEN", "")
+
                 self._signalr_client = SignalRClient(
                     signalr_url,
+                    access_token_factory=token_factory,
                     headers={
                         "X-UiPath-Internal-TenantId": self.context.trace_context.tenant_id,  # type: ignore
                         "X-UiPath-Internal-AccountId": self.context.trace_context.org_id,  # type: ignore
